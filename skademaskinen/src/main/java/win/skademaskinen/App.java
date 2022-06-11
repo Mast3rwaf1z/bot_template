0b10faa47ad1daa9489bd37d7d05ae0be6f0f406
mast3r_waf1z <thomasmollerjensen@live.dk>
2022-06-11 22:02:05 +0200

    cleaned template of bloated stuff

--- skademaskinen/src/main/java/win/skademaskinen/App.java
+++ skademaskinen/src/main/java/win/skademaskinen/App.java
@@ -35,10 +35,6 @@ public class App
         }*/
         jda.updateCommands().addCommands(
             Commands.slash("ping", "send a pong back"),
-            Commands.slash("jail", "Send a user to jail")
-                .addOption(OptionType.USER, "who", "Who to jail", true)
-                .addOption(OptionType.INTEGER, "time", "Time(Default: seconds)", true)
-                .addOption(OptionType.STRING, "measurement", "Seconds, minutes, hours, days", false),
             Commands.slash("color", "Pick a color for your name")
                 .addOption(OptionType.STRING, "color", "Pick a color (Options: Green, Red, Blue, Gray, Yellow, Orange, White, Purple, Pink and Darkgreen)", true),
             Commands.slash("play", "Play a song from youtube")
@@ -50,27 +46,7 @@ public class App
             Commands.slash("disconnect", "Disconnect the bot from voice"),
             Commands.slash("pause", "Pause the bot"),
             Commands.slash("clear", "Clear the song queue"),
-            Commands.slash("help", "Show a list of commands"),
-            Commands.slash("announcement", "Compose an announcement message")
-                .addOption(OptionType.ATTACHMENT, "file", "Announcement text in markdown", true),
-            Commands.slash("application", "Send an application to the raid team")
-                .addOption(OptionType.STRING, "name", "Your character name", true)
-                .addOption(OptionType.STRING, "classspec", "Your class and/or spec", true)
-                .addOption(OptionType.INTEGER, "ilvl", "Specify your ilvl", true)
-                .addOption(OptionType.BOOLEAN, "raidtimes", "Will you be able to raid on Wednesdays and Sundays at 19:30 - 22:30 server time?", true),
-            Commands.slash("roll", "roll a d100 for each entry")
-                .addOption(OptionType.STRING, "entry1", "entry", true)
-                .addOption(OptionType.STRING, "entry2", "entry", false)
-                .addOption(OptionType.STRING, "entry3", "entry", false)
-                .addOption(OptionType.STRING, "entry4", "entry", false)
-                .addOption(OptionType.STRING, "entry5", "entry", false)
-                .addOption(OptionType.STRING, "entry6", "entry", false)
-                .addOption(OptionType.STRING, "entry7", "entry", false)
-                .addOption(OptionType.STRING, "entry8", "entry", false)
-                .addOption(OptionType.STRING, "entry9", "entry", false)
-                .addOption(OptionType.STRING, "entry10", "entry", false),
-            Commands.slash("poop", "Get current poop count for you"),
-            Commands.slash("leaderboard", "Get the leaderboard for the current server")
+            Commands.slash("help", "Show a list of commands")
             ).queue();
     }
 }
